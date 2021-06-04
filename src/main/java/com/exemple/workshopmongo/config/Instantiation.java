8bc54ee8d7483a548db3d2fbe52075b9aa8faf13
Davi dos Santos <davi.slima75@gmail.com>
2021-06-04 12:44:23 -0300

    Database instantiation operation

---
+++ src/main/java/com/exemple/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.exemple.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.exemple.workshopmongo.domain.User;
+import com.exemple.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepsitory;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepsitory.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepsitory.saveAll(Arrays.asList(maria, alex, bob));
+	}
+	
+	
+
+}
